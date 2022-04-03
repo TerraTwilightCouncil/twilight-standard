6ac7bd5a56d62e2c21970959e07443816a6080b3
Pakorn Nathong <goddesign11294_2@hotmail.com>
2022-04-03 19:18:45 +0700

    build: add tw prefix to all packages

--- packages/asset-plus/examples/schema.rs
+++ packages/asset-plus/examples/schema.rs
@@ -1,8 +1,8 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use asset_plus::{Asset, AssetInfo};
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use tw_asset_plus::{Asset, AssetInfo};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
