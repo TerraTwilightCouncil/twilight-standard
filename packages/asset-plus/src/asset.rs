ffdc9d8ffe8a56816ff6a1250ad66b96460c8520
Pakorn Nathong <goddesign11294_2@hotmail.com>
2022-04-15 21:32:48 +0700

    feat: derive PartialOrd

--- packages/asset-plus/src/asset.rs
+++ packages/asset-plus/src/asset.rs
@@ -2,7 +2,7 @@ use cosmwasm_std::{Addr, Uint128};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, Default)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, PartialOrd, JsonSchema, Default)]
 pub struct Asset {
     /// Information about an asset stored in a [`AssetInfo`] struct
     pub info: AssetInfo,
@@ -10,7 +10,7 @@ pub struct Asset {
     pub amount: Uint128,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, PartialOrd, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum AssetInfo {
     /// Non-native Token
