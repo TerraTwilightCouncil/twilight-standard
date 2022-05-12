ffc425e9c6f91abb55f45ebc21ce4ef613b6446a
Pakorn Nathong <goddesign11294_2@hotmail.com>
2022-05-12 23:53:52 +0700

    feat: add derive storage key macros for unit enum

--- packages/storage-macros/src/lib.rs
+++ packages/storage-macros/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
 use syn::{
     Ident,
-    __private::{quote::quote, Span},
-    parse_macro_input, ItemStruct,
+    __private::{quote::quote, Span, TokenStream2},
+    parse_macro_input, DeriveInput, Fields, ItemStruct,
 };
 
 #[proc_macro_attribute]
@@ -34,4 +34,86 @@ pub fn index_list_impl(attr: TokenStream, item: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(StorageKey)]
+pub fn derive_storage_key(item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as DeriveInput);
+    let ident = input.ident;
+
+    match input.data {
+        syn::Data::Enum(data_enum) => {
+            let (b, b_rev): (Vec<TokenStream2>, Vec<TokenStream2>) = data_enum
+                .variants
+                .into_iter()
+                .enumerate()
+                .map(|e| {
+                    let id = e.1.ident;
+                    let data = match e.1.fields {
+                        Fields::Unit => format!("{}", e.0),
+                        _ => panic!(
+                            "#[derive(StorageKey)] currently only support unit enum variants"
+                        ),
+                    };
+
+                    (
+                        quote! {
+                            Self::#id => #data
+                        },
+                        quote! {
+                            #data => Self::#id
+                        },
+                    )
+                })
+                .unzip();
+
+            let expanded = quote! {
+                impl #ident {
+                    pub fn as_bytes(&self) -> &[u8] {
+                        match self { #(#b),* }.as_bytes()
+                    }
+
+                    pub fn from_slice(b: &[u8]) -> Self {
+                        match std::str::from_utf8(b).unwrap() {
+                            #(#b_rev),*,
+                            _ => panic!("Should not be others")
+                        }
+                    }
+                }
+
+                impl PrimaryKey<'_> for #ident {
+                    type Prefix = ();
+                    type SubPrefix = ();
+
+                    fn key(&self) -> Vec<&[u8]> {
+                        vec![self.as_bytes()]
+                    }
+                }
+
+                impl<'a> PrimaryKey<'a> for &'a #ident {
+                    type Prefix = ();
+                    type SubPrefix = ();
+
+                    fn key(&self) -> Vec<&[u8]> {
+                        vec![self.as_bytes()]
+                    }
+                }
+
+                impl Prefixer<'_> for #ident {
+                    fn prefix(&self) -> Vec<&[u8]> {
+                        vec![self.as_bytes()]
+                    }
+                }
+
+                impl<'a> Prefixer<'a> for &'a #ident {
+                    fn prefix(&self) -> Vec<&[u8]> {
+                        vec![self.as_bytes()]
+                    }
+                }
+            };
+
+            TokenStream::from(expanded)
+        }
+        _ => panic!("#[derive(StorageKey)] currently only support enums."),
+    }
 }
